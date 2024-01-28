61224866aa4d4c6992e1ded41949a5cabb8cadd8
leila <143362178+1e1104k4@users.noreply.github.com>
2024-01-28 17:23:28 -0600

    Initial Commit

---
+++ Bookwarm/BookwarmApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwarmApp.swift
+//  Bookwarm
+//
+//  Created by Leila on 1/28/24.
+//
+
+import SwiftUI
+
+@main
+struct BookwarmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
