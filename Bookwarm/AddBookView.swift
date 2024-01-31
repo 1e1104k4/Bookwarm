c03c9988003b0fa2a85c63c608b229a823c28f48
leila <143362178+1e1104k4@users.noreply.github.com>
2024-01-31 14:43:25 -0600

    asset images for genres

--- Bookwarm/AddBookView.swift
+++ Bookwarm/AddBookView.swift
@@ -17,7 +17,7 @@ struct AddBookView: View {
     @State private var review = ""
     @State private var rating = 3
     
-    let genres = ["Fiction", "Fantasy", "Horror", "Kids", "Mystery", "Poetry", "Romance", "Thriller"]
+    let genres = ["Fantasy", "Horror", "Kids", "Mystery", "Poetry", "Romance", "Thriller"]
     
     var body: some View {
         NavigationStack {
